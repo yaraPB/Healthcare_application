b9f74eb9976fe6f5ed3d50227eebf138dc44e721
yaraPB <yarakouttane234@gmail.com>
2024-10-26 19:42:13 +0100

    reinitiliaze all of it

--- f1/index.js
+++
@@ -1,8 +0,0 @@
-// command to install express: npm i express
-const express = require('express')
-const app = express()
-
-  app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
-    
-  })
